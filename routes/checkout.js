beb542747e94858a684692e31e1bbca17dec631f
Minami Sato <minami10076@gmail.com>
2021-12-09 15:47:58 -0800

    add bootstrap to all pages

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'text/html');
     res.write("<title>Grocery CheckOut Line</title>");
+    res.write('<head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> </head>');
 
     res.write("<h1>Enter your customer id to complete the transaction:</h1>");
 
